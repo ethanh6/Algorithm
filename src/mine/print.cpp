872ddd4a2fbc3f4d84ac656937374ef9f3eb734e
ethanhuang <ethanhuang0606@gmail.com>
2023-03-27 01:01:54 -0700

    add auto run

--- src/mine/print.cpp
+++ src/mine/print.cpp
@@ -1,8 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
 #include <vector>
 #include <iostream>
+#include <sstream>
 using std::vector;
 
+
+// print 1d and 2d generic vector
 template <typename T> void print_vector(const T &t) {
   std::copy(t.cbegin(), t.cend(),
             std::ostream_iterator<typename T::value_type>(std::cout, ", "));
@@ -13,8 +16,28 @@ template <typename T> void print_2d_vector(const T &t) {
   std::for_each(t.cbegin(), t.cend(), print_vector<typename T::value_type>);
 }
 
-TEST_CASE("test") {
-  vector<int> v{1, 2, 3};
+// testing
+class CoutRedirector {
+ public:
+  CoutRedirector(std::streambuf* new_buffer)
+      : old_buffer_(std::cout.rdbuf(new_buffer)) {}
+
+  ~CoutRedirector() {
+    std::cout.rdbuf(old_buffer_);
+  }
+
+ private:
+  std::streambuf* old_buffer_;
+};
+
+
+
+TEST_CASE("Test cout", "[test cout]") {
+  std::stringstream buffer;
+  CoutRedirector redirector(buffer.rdbuf());
+
+  vector<int> v {1, 2, 3};
   print_vector(v);
-  REQUIRE(true);
+
+  REQUIRE(buffer.str() == "1, 2, 3, \n");
 }
